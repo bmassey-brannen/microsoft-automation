84f9b0f300181fcea22f8c1ae272bdef4f6ba05e
Benjamin Brannen <144388461+bmassey-brannen@users.noreply.github.com>
2024-02-26 11:26:19 -0500

    Create runAllCommands.ps1

---
+++ runAllCommands.ps1
@@ -0,0 +1,8 @@
+# Change directory
+Set-Location -Path "C:\Users\<Folder Path>"
+
+# Run stepOneEmailAutomation.ps1
+.\stepOneEmailAutomation.ps1
+
+# Run recalDailyValue.py
+python .\stepTwoFormatAndMove.py
